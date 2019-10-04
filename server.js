0adfac2ebed184b473be74fb811fde3ef0eaf8e0
riverhuang-batam <riverhuang011@gmail.com>
2019-10-04 19:07:44 +0700

    learning display table/ select & insert using mysql node js express framework

---
+++ server.js
@@ -0,0 +1,81 @@
+//import all node modules 
+const express = require('express');
+const mysql = require('mysql');
+const bodyparser = require('body-parser');
+const http = require('http')
+var app = express();
+
+//parse all form data
+app.use(bodyparser.urlencoded({extended: true}));
+//formating date
+var dateFormat = require('dateformat');
+var nowDate = new Date(); 
+//view engine for showing view
+app.set('view engine', 'ejs')
+app.set('views', './view')
+
+//importing file tha gonna use
+// using bootstrap to design the website
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/tehter/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist/js'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+const siteTitle = "EventIF";
+const baseURL = "http://localhost:3000/"
+
+
+const mysqlConnection = mysql.createConnection({
+    host:'localhost',
+    user : 'root',
+    password: 'Firewall@421',
+    database: 'EmployeeDB'
+})
+
+mysqlConnection.connect((err)=>{
+    if(!err)
+        console.log('DB connection succeded.');
+    
+    else
+        console.log('DB connection failed \n Error:' + JSON.stringify(err, undefined, 2));
+    
+})
+
+app.get('/',(req,res)=>{
+    
+    mysqlConnection.query(`SELECT * FROM Employee`,(err, rows, fields)=>{
+        console.log(fields)
+        res.render("pages/index",{
+            siteTitle: siteTitle,
+            pageTitle: "Event list",
+            items : rows
+        });    
+        if(!err)
+        
+            console.log(rows);
+            
+            else
+            console.log(err)
+    });
+})
+app.get('/eventif/addemployee',(req,res)=>{
+    res.render("pages/addemployee",{
+        siteTitle: siteTitle,
+        pageTitle: "Add new employee",
+        items: ''
+    })
+})
+
+//post method
+app.post('/eventif/addemployee',(req,res)=>{
+    var query = "INSERT INTO `Employee`(Name, EmpCode, Salary) VALUES (";
+    
+        query += " '"+req.body.Name+"',";
+        query += " '"+req.body.EmpCode+"',";
+        query += " '"+req.body.Salary+"')";
+    mysqlConnection.query(query, (err,rows)=>{
+        res.redirect(baseURL);
+    });
+});
+//connect to the server 
+app.listen(3000,()=> console.log('Express server is running in 3000'));
